02bca989aa464459519e16b66f755d182a3d4ea0
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-14 16:16:56 +0500

    Add config for logging; add func get_log_config

--- server/config/log_config.py
+++ server/config/log_config.py
@@ -1,1 +1,48 @@
 """The module responsible for configuring logging."""
+
+from copy import deepcopy
+from typing import Any, Dict
+
+from .app_config import Config
+
+LOG_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "base": {"format": "[%(levelname)s] [%(asctime)s] | %(name)s - %(message)s"},
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "base",
+        },
+        "file": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "level": "INFO",
+            "formatter": "base",
+            "filename": "logfile.log",
+            "backupCount": 3,
+            "when": "d",
+            "interval": 10,
+            "encoding": "utf-8",
+        },
+    },
+    "loggers": {
+        "main": {
+            "level": "DEBUG",
+            "handlers": ["file", "console"],
+            "propagate": False,
+        },
+    },
+}
+
+
+def get_log_config(config: Config) -> Dict[str, Any]:
+    """Return config for logging."""
+    if not config.debug:
+        log_config: Dict[str, Any] = deepcopy(LOG_CONFIG)
+        log_config["handlers"]["console"]["level"] = "INFO"
+        log_config["loggers"]["main"]["level"] = "INFO"
+        return log_config
+    return LOG_CONFIG
