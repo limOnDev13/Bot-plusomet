b331b6bd55b29374273df3b80f0bb491dab9e986
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-14 18:57:06 +0500

    Create dataclass Prompt; create abstract class BaseLLMAPI; add abstract method send_prompts;

--- server/services/api/llm/base.py
+++ server/services/api/llm/base.py
@@ -1,1 +1,25 @@
 """The module responsible for the basic interface of interaction with LLM."""
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Dict, List, Literal
+
+
+@dataclass
+class Prompt(object):
+    """Class representing the industrial structure."""
+
+    role: Literal["user", "system", "assistant"]
+    text: str
+
+    def to_dict(self) -> Dict[str, str]:
+        return {"role": self.role, "text": self.text}
+
+
+class BaseLLMAPI(ABC):
+    """The basic interface for working with LLM."""
+
+    @abstractmethod
+    def send_prompts(self, chat: List[Prompt]) -> List[Prompt]:
+        """Send messages to LLM."""
+        pass
