33369a654bd18a5f1c81fcfb97d0c015c2ee6c90
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-14 18:58:21 +0500

    Create dataclass YandexGPTConfig for config working with YandexGPT

--- server/config/app_config.py
+++ server/config/app_config.py
@@ -5,14 +5,31 @@ from dataclasses import dataclass
 
 
 @dataclass
+class YandexGPTConfig(object):
+    """Config for work with YandexGPT API."""
+
+    iam_token: str
+    catalog_id: str
+    temperature: float
+    max_tokens: int
+
+
+@dataclass
 class Config(object):
     """Config class for the app."""
 
     debug: bool
+    yandex_gpt: YandexGPTConfig
 
 
 def get_config() -> Config:
     """Return app config."""
     return Config(
         debug=os.getenv("SERVER_DEBUG", "0") == "1",
+        yandex_gpt=YandexGPTConfig(
+            iam_token=os.getenv("YANDEXGPT_IAM", ""),
+            catalog_id=os.getenv("YANDEXGPT_CATALOG_ID", ""),
+            temperature=float(os.getenv("YANDEXGPT_TEMPERATURE", 0.3)),
+            max_tokens=int(os.getenv("YANDEXGPT_MAX_TOKENS", 2000)),
+        ),
     )
