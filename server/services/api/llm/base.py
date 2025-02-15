210f861dd219efada3a41fa2356c789856314943
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-15 11:56:45 +0500

    Create module prompts.py; move class Prompt from base.py to prompts.py; add dict PROMPTS with prompts

--- server/services/api/llm/base.py
+++ server/services/api/llm/base.py
@@ -1,20 +1,9 @@
 """The module responsible for the basic interface of interaction with LLM."""
 
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from typing import Dict, List, Literal
+from typing import List
 
-
-@dataclass
-class Prompt(object):
-    """Class representing the industrial structure."""
-
-    role: Literal["user", "system", "assistant"]
-    text: str
-
-    def to_dict(self) -> Dict[str, str]:
-        """Return dict with fields from class."""
-        return {"role": self.role, "text": self.text}
+from server.services.prompts import Prompt
 
 
 class BaseLLMAPI(ABC):
