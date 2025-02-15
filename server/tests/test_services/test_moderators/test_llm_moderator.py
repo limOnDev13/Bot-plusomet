11172e3e21b89428bfda0a622438d721d34fdffe
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-15 14:18:12 +0500

    Create module test_llm_moderator.py; add test test_llm_moderator

---
+++ server/tests/test_services/test_moderators/test_llm_moderator.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from server.services.moderators.llm_moderator import LLMModerator, ModerationResult
+
+
+def test_llm_moderator(
+    llm_moderator: LLMModerator,
+    user_msgs: List[str],
+    llm_generated_msgs: List[str],
+    toxic_msgs: List[str],
+) -> None:
+
+    for msg in user_msgs:
+        result: ModerationResult = llm_moderator.moderate(msg)
+        print(msg)
+        assert result.generated_by_llm is False
+        assert result.toxic is False
+
+    for msg in llm_generated_msgs:
+        result: ModerationResult = llm_moderator.moderate(msg)
+        print(msg)
+        assert result.generated_by_llm is True
+
+    for msg in toxic_msgs:
+        result: ModerationResult = llm_moderator.moderate(msg)
+        print(msg)
+        assert result.toxic is True
