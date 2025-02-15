1262327026663e0e8401b6dd818c5ea2a39d2a8b
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-15 14:15:35 +0500

    Add exceptions APIAuthException, PromptError, IncorrectEncodingError, IncorrectFormatError

--- server/services/excs.py
+++ server/services/excs.py
@@ -1,10 +1,43 @@
 """The module responsible for custom exceptions."""
 
 import json
-from typing import Optional
+from typing import Any, List, Optional
+
+from server.services.prompts import Prompt
 
 
-class APIException(Exception):
+class APIAuthException(Exception):
+    """The exception is for errors related to auth on third-party APIs."""
+
+    def __init__(
+        self,
+        service_name: str,
+        msg: Optional[str] = None,
+        json_str: Optional[str] = None,
+    ):
+        """
+        Init class.
+
+        :param service_name: Name of API service.
+        :param msg: Message from server.
+        :param json_str: JSON from server.
+        """
+        self.service_name = service_name
+        self.msg = msg
+        self.json_str = json_str
+
+    def __str__(self) -> str:
+        """Return str of exception."""
+        res_str: str = f"Service: {self.service_name}"
+        if self.msg:
+            res_str += ", ".join((res_str, self.msg))
+        if self.json_str:
+            res_str += ", ".join((res_str, json.dumps(self.json_str, indent=2)))
+
+        return res_str
+
+
+class APIException(APIAuthException):
     """The exception is for errors from third-party APIs."""
 
     def __init__(
@@ -22,17 +55,66 @@ class APIException(Exception):
         :param msg: Message from server.
         :param json_str: JSON from server.
         """
-        self.service_name = service_name
+        super().__init__(service_name, msg, json_str)
         self.status_code = status_code
-        self.msg = msg
-        self.json_str = json_str
 
     def __str__(self) -> str:
         """Return str of exception."""
-        res_str: str = f"Service: {self.service_name}, Status code: {self.status_code}"
-        if self.msg:
-            res_str += ", ".join((res_str, self.msg))
-        if self.json_str:
-            res_str += ", ".join((res_str, json.dumps(self.json_str, indent=2)))
+        res_str: str = super().__str__()
 
-        return res_str
+        return ", ".join((f"Status code: {self.status_code}", res_str))
+
+
+class PromptError(Exception):
+    """The exception is for errors related to incorrect prompt logic."""
+
+    def __init__(self, prompts: List[Prompt], msg: Optional[str] = None):
+        """
+        Init exception.
+
+        :param prompts: Chat history.
+        :param msg: Exception message.
+        """
+        self.prompts = prompts
+        self.msg = msg
+
+    def __str__(self) -> str:
+        """Return str of exception."""
+        return f"{self.msg}\nChat history:\n{json.dumps(self.prompts, indent=2)}"
+
+
+class IncorrectEncodingError(PromptError):
+    """The exc responsible for errors related to incorrect encoding of the LLM answer."""
+
+    pass
+
+
+class IncorrectFormatError(PromptError):
+    """The exc responsible for errors related to the invalid answer format from LLM."""
+
+    def __init__(
+        self,
+        prompts: List[Prompt],
+        msg: Optional[str] = None,
+        expected: Any = None,
+        received: Any = None,
+    ):
+        """
+        Init exception.
+
+        :param prompts: Chat history.
+        :param msg: Message.
+        :param expected: Expected format of example of correct answer.
+        :param received: Received answer.
+        """
+        super().__init__(prompts=prompts, msg=msg)
+        self.expected = expected
+        self.received = received
+
+    def __str__(self) -> str:
+        """Return str of exception."""
+        res_str: str = super().__str__()
+        if self.expected is not None:
+            return "\n".join(
+                (res_str, f"Expected:\n{self.expected}\nReceived:\n{self.received}")
+            )
