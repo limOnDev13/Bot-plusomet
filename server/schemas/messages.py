cb07f5bb41b2eb2544456d6dfd3af35ee07c8e4b
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-17 17:55:07 +0500

    Create schemas MessageSchema, ModerationResultSchema

---
+++ server/schemas/messages.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class MessageSchema(object):
+    """The scheme of the message for subsequent moderation."""
+
+    id: str
+    text: str
+
+
+@dataclass
+class ModerationResultSchema(object):
+    """The structure of the LLM response after message moderation."""
+
+    msg_id: str
+    generated_by_llm: bool
+    toxic: bool
