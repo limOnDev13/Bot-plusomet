6c5de9eeb81f11be1e014872f08b6cb86154d53f
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-17 19:23:32 +0500

    Fix __init__ method - add raising ValueError, if random delay has incorrect format; fix method moderate - add delay

--- server/services/moderators/llm_moderator.py
+++ server/services/moderators/llm_moderator.py
@@ -1,10 +1,12 @@
 """The module responsible for moderation through LLM."""
 
 import json
+import random
+import time
 from dataclasses import asdict
 from json import JSONDecodeError
 from logging import getLogger
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 
 from server.schemas.messages import MessageSchema, ModerationResultSchema
 from server.services.api.llm.base import BaseLLMAPI
@@ -21,13 +23,19 @@ logger = getLogger("main.services.moderator")
 class LLMModerator(object):
     """The class responsible for moderation of messages using LLM."""
 
-    def __init__(self, llm_api: BaseLLMAPI):
+    def __init__(self, llm_api: BaseLLMAPI, random_delay: Tuple[float, float] = (1, 3)):
         """
         Init class.
 
         :param llm_api: BaseLLMAPI object.
+        :param random_delay: The boundaries of the random delay value.
+        :raise ValueError: If random_delay[0] > random_delay[1]
         """
         self.llm_api = llm_api
+
+        if random_delay[0] > random_delay[1]:
+            raise ValueError(f"{random_delay[0]=} must be no more {random_delay[1]=}")
+        self.__random_delay = random_delay
 
     def moderate(self, message: MessageSchema) -> ModerationResultSchema:
         """
@@ -78,3 +86,5 @@ class LLMModerator(object):
                 ),
                 received=json.dumps(answer_dict, indent=2),
             )
+        finally:
+            time.sleep(random.uniform(*self.__random_delay))
