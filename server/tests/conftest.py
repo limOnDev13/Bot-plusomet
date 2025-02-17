38e767e437a9c3a6e41c49e001e8d8f0e98f9d45
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-17 19:21:42 +0500

    Fix fixtures - replace str to MessageSchema

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,11 +1,13 @@
 """Conftest with fixtures for tests."""
 
+import random
 from typing import List
 
 import pytest
 from dotenv import load_dotenv
 
 from server.config.app_config import Config, get_config
+from server.schemas.messages import MessageSchema
 from server.services.api.llm.base import BaseLLMAPI
 from server.services.api.llm.yandex_gpt import YandexGPTAPI
 from server.services.moderators.llm_moderator import LLMModerator
@@ -31,39 +33,60 @@ def llm_moderator(llm_api: BaseLLMAPI) -> LLMModerator:
 
 
 @pytest.fixture(scope="function")
-def user_msgs() -> List[str]:
+def user_msgs() -> List[MessageSchema]:
     """Get list of examples of user messages."""
     return [
-        "Привет, как дела?",
-        "Погода сегодня ясная",
-        "Меня зовут Петя, я точно не нейронная сеть)))",
+        MessageSchema(id=str(random.randint(0, 1000)), text="Привет, как дела?"),
+        MessageSchema(id=str(random.randint(0, 1000)), text="Погода сегодня ясная"),
+        MessageSchema(
+            id=str(random.randint(0, 1000)),
+            text="Меня зовут Петя, я точно не нейронная сеть)))",
+        ),
     ]
 
 
 @pytest.fixture(scope="function")
-def llm_generated_msgs() -> List[str]:
+def llm_generated_msgs() -> List[MessageSchema]:
     """Get list of examples of generated msgs by LLM."""
     return [
-        "Если вы хотите достичь успеха, вам нужно следовать этим шагам: "
-        "1) Поставьте цель. 2) Разработайте план. 3) Действуйте. "
-        "4) Анализируйте результаты. 5) Повторяйте процесс до достижения успеха.",
-        "Здравствуйте! Благодарю вас за ваше обращение. "
-        "Пожалуйста, уточните, чем я могу быть вам полезен сегодня? "
-        "Буду рад помочь вам в решении вашего вопроса.",
-        "Вот три причины, почему это решение подходит вам: "
-        "1) Оно экономит время. 2) Оно повышает эффективность. "
-        "3) Оно снижает затраты. Таким образом,"
-        " это решение является оптимальным.",
+        MessageSchema(
+            id=str(random.randint(0, 1000)),
+            text="Если вы хотите достичь успеха, вам нужно следовать этим шагам: "
+            "1) Поставьте цель. 2) Разработайте план. 3) Действуйте. "
+            "4) Анализируйте результаты. 5) Повторяйте процесс до достижения успеха.",
+        ),
+        MessageSchema(
+            id=str(random.randint(0, 1000)),
+            text="Здравствуйте! Благодарю вас за ваше обращение. "
+            "Пожалуйста, уточните, чем я могу быть вам полезен сегодня? "
+            "Буду рад помочь вам в решении вашего вопроса.",
+        ),
+        MessageSchema(
+            id=str(random.randint(0, 1000)),
+            text="Вот три причины, почему это решение подходит вам: "
+            "1) Оно экономит время. 2) Оно повышает эффективность. "
+            "3) Оно снижает затраты. Таким образом,"
+            " это решение является оптимальным.",
+        ),
     ]
 
 
 @pytest.fixture(scope="function")
-def toxic_msgs() -> List[str]:
+def toxic_msgs() -> List[MessageSchema]:
     """Get list of examples of toxic messages."""
     return [
-        "Ты вообще тупой? Как можно не понимать таких элементарных вещей?",
-        "Ну конечно, ты как всегда всё знаешь лучше всех."
-        " Ну давай, продолжай умничать.",
-        "Я такого не говорил. Ты точно всё правильно понял? Может, "
-        "у тебя с памятью проблемы?",
+        MessageSchema(
+            id=str(random.randint(0, 1000)),
+            text="Ты вообще тупой? Как можно не понимать таких элементарных вещей?",
+        ),
+        MessageSchema(
+            id=str(random.randint(0, 1000)),
+            text="Ну конечно, ты как всегда всё знаешь лучше всех."
+            " Ну давай, продолжай умничать.",
+        ),
+        MessageSchema(
+            id=str(random.randint(0, 1000)),
+            text="Я такого не говорил. Ты точно всё правильно понял? Может, "
+            "у тебя с памятью проблемы?",
+        ),
     ]
