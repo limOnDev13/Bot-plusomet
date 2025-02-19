1842c42d0a0f23fcff55555d2798d278504484b7
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-02-19 17:18:49 +0500

    Add docstrings

--- server/services/moderators/base.py
+++ server/services/moderators/base.py
@@ -1,10 +1,14 @@
+"""The module responsible for the interface of all moderators."""
+
 from abc import ABC, abstractmethod
 
 from schemas.messages import MessageSchema, ModerationResultSchema
 
 
 class BaseModerator(ABC):
+    """The moderator's basic interface."""
 
     @abstractmethod
     def moderate(self, msg: MessageSchema) -> ModerationResultSchema:
+        """Moderate msg."""
         pass
